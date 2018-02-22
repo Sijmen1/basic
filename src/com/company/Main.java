4efc4b269ff854e44abf874920f51f38a3c91da3
Sijmen <sijmen.jaarsma@student.hu.nl>
2018-02-22 14:36:18 +0100

    added a useless change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("first commit");
+        System.out.println("first commit v1");
     }
 }
