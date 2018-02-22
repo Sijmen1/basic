7efb0ace00d6b94a815e9eee1447445da4d878c3
Sijmen <sijmen.jaarsma@student.hu.nl>
2018-02-22 14:54:25 +0100

    Commit number 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("first commit v1");
+        System.out.println("I would be happier if I could sleep the day away");
     }
 }
