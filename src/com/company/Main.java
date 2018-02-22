9cf6626c57a51bee6abdb7dd96fee4e79a2320f0
Sijmen <sijmen.jaarsma@student.hu.nl>
2018-02-22 14:50:06 +0100

    commit number 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("first commit v1");
+        System.out.println("I don't feel like doing this workshop but I have to because it is 5 percent of my grade");
     }
 }
