df9f932d03e4aeae864364f2537e36b0f9ff4884
Sijmen1 <sijmenj@gmail.com>
2018-02-23 01:50:13 +0100

    Update Main.java
    check data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("first csat v1");
+        System.out.println("first csat vegaegwa1");
         System.out.println("I don't feel like doing this workshop but I have to because it is 5 percent of my grade");
 
         System.out.println("I would be happier if I could sleep the day away");
